ce23c40889f5eda6d7962bab0848c5d663d431d3
Nareshsky <123462633+Nareshsky@users.noreply.github.com>
2023-05-09 12:08:22 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,4 +1,4 @@
 #All infrastructure will be on the AWS. If you want to use another cloud provider such as GCP or Azure, you need to change this.
 provider "aws" {
-    region = "US-east-2"
+    region = "us-east-1"
 }

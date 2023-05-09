c732b9dfe4012cdc15930a9078a6802f593f0603
Nareshsky <123462633+Nareshsky@users.noreply.github.com>
2023-05-09 11:27:21 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,4 @@
+#All infrastructure will be on the AWS. If you want to use another cloud provider such as GCP or Azure, you need to change this.
+provider "aws" {
+    region = "${var.AWS_REGION}"
+}

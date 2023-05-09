bfc5ce6712e73c9a8e6ded1faa23d6509f17bb7b
Nareshsky <123462633+Nareshsky@users.noreply.github.com>
2023-05-09 11:58:26 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,13 +1,12 @@
 ##Create vpc
 
-resource “aws_vpc” “prod-vpc” {
+resource “aws_vpc” “demo-vpc” {
     cidr_block = “10.0.0.0/16”
     enable_dns_support = “true” #gives you an internal domain name
     enable_dns_hostnames = “true” #gives you an internal host name
     enable_classiclink = “false”
     instance_tenancy = “default”    
     
-    tags {
-        Name = “prod-vpc”
+    
     }
 }
